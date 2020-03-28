2d055cc897cf48da51f3b24e694a9a885fcb9986
geoffreylu8 <jgl3@williams.edu>
2020-03-28 16:37:23 -0700

    added basic map

--- PeopleView/App.js
+++ PeopleView/App.js
@@ -1,4 +1,33 @@
 import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.mapStyle} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
+
+{/*
+
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -17,3 +46,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+*/}
